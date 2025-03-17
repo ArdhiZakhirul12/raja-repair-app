934abb62f09bf577f0d404689fdd0117a55c39f8
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2025-03-17 08:10:07 +0700

    update login background

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <img src="{{ asset('images/raja_repair.svg') }}" alt="logo" class="h-20">
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
