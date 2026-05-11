ff904f40074510b04ebd75aaa128d26b866c04bf
ArdhiZakhirul12 <53462558+ArdhiZakhirul12@users.noreply.github.com>
2026-05-11 17:17:22 +0700

    Update detail-booking.blade.php

--- resources/views/livewire/detail-booking.blade.php
+++ resources/views/livewire/detail-booking.blade.php
@@ -118,13 +118,15 @@
                         selesaikan pesanan dibatalkan
                     </button>
                 @endif
+                <button 
+    class="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700"
+    onclick="window.location.href='{{ route('print.nota', $booking->id) }}'">
+    Cetak Invoice
+</button>
 
                 {{-- @dd($upDokumen) --}}
                 @if ($dokumenStatus == 0 && $booking->status != 'teknisi-batal')
-                    <button class="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700"
-                        onclick="printDiv('struk-pembayaran')">
-                        Cetak Invoice
-                    </button>
+                    
                     <button class="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700"
                         wire:click="$set('isModalDokumen', true)">
                         Upload Dokumen
